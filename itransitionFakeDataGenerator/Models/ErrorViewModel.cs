2fd7dd370b71572cf9b5effb4fb9b8089e7813a9
VictorBrito13 <javibrito10@gmail.com>
2024-09-18 18:40:21 -0600

    project init

---
+++ itransitionFakeDataGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace itransitionFakeDataGenerator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
